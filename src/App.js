cfb53784867d1ff926b3677be3f4ab4f4dc1d1f1
adarsh9523v <167247778+adarsh9523v@users.noreply.github.com>
2024-06-23 15:09:05 +0530

    styled expenses using nested components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App(){
     let expenses = [
@@ -27,31 +27,12 @@ function App(){
             amount: 500,
             date: new Date(2021, 4, 19)
         }
-    ]
+    ];
 
     return( 
         <div>
             <h2>Let's get started</h2>
-            <ExpenseItem
-                date={ expenses[0].date }
-                title={ expenses[0].title } 
-                amount={ expenses[0].amount }>
-            </ExpenseItem>
-            <ExpenseItem
-                date={ expenses[1].date }
-                title={ expenses[1].title } 
-                amount={ expenses[1].amount }>
-            </ExpenseItem>
-            <ExpenseItem
-                date={ expenses[2].date }
-                title={ expenses[2].title } 
-                amount={ expenses[2].amount }>
-            </ExpenseItem>
-            <ExpenseItem
-                date={ expenses[3].date }
-                title={ expenses[3].title } 
-                amount={ expenses[3].amount }>
-            </ExpenseItem>v
+            <Expenses item={expenses} />
         </div>
     );
 }
