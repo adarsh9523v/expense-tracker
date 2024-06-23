184d7c653a877d6a03ca93432a9320f354f41551
adarsh9523v <167247778+adarsh9523v@users.noreply.github.com>
2024-06-23 12:25:17 +0530

    styled date format using multiple components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import './App.css'
 
-import SubComponent from './components/SubComponents';
+import ExpenseItem from './components/ExpenseItem';
 
 function App(){
-    return(  
-            <SubComponent/>
+    let expenses = [
+        {
+            id: 'e1',
+            title: 'school fee',
+            amount: 250,
+            date: new Date(2021, 5, 12)
+        },
+        {
+            id: 'e2',
+            title: 'Books',
+            amount: 20,
+            date: new Date(2022, 2, 16)
+        },
+        {
+            id: 'e3',
+            title: 'House Rent',
+            amount: 700,
+            date: new Date(2021, 10, 17)
+        },
+        {
+            id: 'e4',
+            title: 'Food',
+            amount: 500,
+            date: new Date(2021, 4, 19)
+        }
+    ]
+
+    return( 
+        <div>
+            <h2>Let's get started</h2>
+            <ExpenseItem
+                date={ expenses[0].date }
+                title={ expenses[0].title } 
+                amount={ expenses[0].amount }>
+            </ExpenseItem>
+            <ExpenseItem
+                date={ expenses[1].date }
+                title={ expenses[1].title } 
+                amount={ expenses[1].amount }>
+            </ExpenseItem>
+            <ExpenseItem
+                date={ expenses[2].date }
+                title={ expenses[2].title } 
+                amount={ expenses[2].amount }>
+            </ExpenseItem>
+            <ExpenseItem
+                date={ expenses[3].date }
+                title={ expenses[3].title } 
+                amount={ expenses[3].amount }>
+            </ExpenseItem>v
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import './index.css'
 import App from './App';
 
 
-ReactDom.render(<App />, document.getElementById('root')); 
+ReactDom.render(<App />, document.getElementById('root'));
